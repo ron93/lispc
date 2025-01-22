45aae4af3d993b060db061d1c64f596d61b16ec9
ron93 <ronmkare1@gmail.com>
2025-01-22 13:24:17 +0300

    parse polish notation based language

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include  "mpc.h"
+
 #define MAXIMUM_BUFFER_SIZE 2048
 /* compile this functions if on  windows*/
 #ifdef _WIN32
@@ -30,6 +30,22 @@ void add_history(char* unused) {}
 
 int main(int arg, char**argv)
 {
+    /* Parsers*/
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr = mpc_new("expr");
+    mpc_parser_t* Lispc = mpc_new("lispc");
+
+    /* define language*/
+    mpca_lang(MPCA_LANG_DEFAULT, 
+        "                                                     \
+            number   : /-?[0-9]+/ ;                             \
+            operator : '+' | '-' | '*' | '/' ;                  \
+            expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+            lispc    : /^/ <operator> <expr>+ /$/ ;             \
+        ",
+        Number, Operator, Expr, Lispc);
+
     puts("lispc version 0.0.0.0.1"); /* version info */
     puts("Press ctrl+c to exit\n");
 
@@ -38,9 +54,20 @@ int main(int arg, char**argv)
         char* input = readline("lispc> ");
         add_history(input); /*add input to history-> accessed by pressing the arrow key*/
 
-        printf("No you're a %s\n", input);
+        /* parse input*/
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Lispc, &r)) {
+            /* print and delete AST*/
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } 
+        else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
 
         free(input);
     }
+    mpc_cleanup(4, Number, Operator, Expr, Lispc);
     return 0;
 }
