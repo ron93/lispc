d759b89b2d82b74f72118ee4258896b849975480
ron93 <ronmkare1@gmail.com>
2025-01-28 13:31:03 +0300

    added fucntions to read lval number and rest of expression

--- main.c
+++ main.c
@@ -104,6 +104,32 @@ void lval_del(lval* v) {
     }
     free(v);
 }
+
+lval* lval_read_num(mpc_ast_t* t) {
+    errno = 0;
+    long x = strtol(t->contents, NULL, 10);
+    return errno != ERANGE ? lval_num(x) : lval_err("invalid number");
+}
+
+lval* lval_read(mpc_ast_t* t) {
+    /* check value and convert to target type */
+    if (strstr(t->tag, "number")) { return lval_read_num(t);}
+    if (strstr(t->tag, "symbol")) { return lval_sym(t->contents); }
+
+    /* if root(>) or sexpr create empty list */
+    lval* x = NULL;
+    if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
+    if (strstr(t->tag, "sexpr")) { x = lval_sexpr(); }
+
+    /* fill list with valid expresion */
+    for (int i = 0; i < t->children_num; i++) {
+        if (strcmp(t->children[i]->contents, "(") == 0) { continue; }
+        if (strcmp(t->children[i]->contents, ")") == 0) { continue; }
+        if (strcmp(t->children[i]->tag, "regex") == 0) { continue; }
+        x = lval_add(x, lval_read(t->children[i]));
+    }
+    return x;
+}
 /* print lval */
 void lval_print(lval v) {
     switch (v.type) {
